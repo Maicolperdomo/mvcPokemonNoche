78d785e01d05e205e180ef6cfc4ed1ef5e57119d
Maicol Perdomo <maicolfabianp79@gmail.com>
2023-05-10 19:12:11 -0500

    Primer Commit MVC

---
+++ view/roles.frm.php
@@ -0,0 +1,20 @@
+<?php include_once "header.php"; ?>
+<div class="row  my-3">
+    <h1 class="text-center">Formulario Roles</h1>
+</div>
+<div class="row">
+    <div class="col-3 d-flex justify-content-end align-items-center">
+        <span class="fw-bolder">Nombre Rol:</span>
+    </div>
+    <div class="col-7">
+        <div class="form-floating">
+            <input type="text" class="form-control" id="floatingInput" placeholder="Hola">
+            <label for="floatingInput">Nombre Rol</label>
+        </div>
+    </div>
+    <div class="row bg-dark mt-5">
+        <h1 class="text-white text-center">Tabla de Roles</h1>
+    </div>
+</div>
+
+<?php include_once "footer.php"; ?>
